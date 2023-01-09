f5e20f223296b8cfba991f081660fb64eac90f20
AakashHelloworldgit config --global user.name AakashHelloworldgit config --global user.name AakashHelloworld <aakashbandhuaryal@gmail.com>
2023-01-09 13:37:52 +0545

    Sign up and login page is updated! :::=> Aakash

---
+++ pages/login.js
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+import LoginComponent from "../components/LoginComponent"
+
+const login = () => {
+  return (
+    <div>
+        <Navbar/>
+        <LoginComponent />
+        <Footer/>
+    </div>
+  )
+}
+
+export default login
---
+++ pages/signup.js
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer"
+import { SignUpComponent } from "../components/SignUpComponent"
+import Navbar from "../components/Navbar"
+
+const SignUp = () => {
+  return (
+    <div>
+      <Navbar />
+      <SignUpComponent/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default SignUp
