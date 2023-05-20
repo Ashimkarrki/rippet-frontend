a5b03cfc561ab90c0cce8977131cca8e1b240f9b
ashimkarrki <ashimkarki987@gmail.com>
2023-05-20 12:45:46 +0545

    review

--- pages/sellerDashboard/reviews/index.js
+++ pages/sellerDashboard/reviews/index.js
@@ -21,48 +21,23 @@ const SellerReviews = () => {
         try {
           const res = await instance.get(url);
           console.log(res.data.data);
-          const result = await axios.get(
-            `https://adorable-leather-jacket-foal.cyclic.app/api/v1/products/${res.data.data[0].product}`
+          setProductReviewInfo(
+            res.data.data.map((s) => {
+              return {
+                id: s.id,
+                rating: s.rating,
+                review: s.review,
+                reviewer: s.user.Username,
+                userId: s.user.id,
+                date: s.createdAt,
+                MainImage: s.product.MainImage,
+                productName: s.product.Name,
+                productPrice: s.product.Price,
+                productAvgRating: s.product.AverageRating,
+              };
+            })
           );
-          // console.log(res.data.data);
-          setProductReviewInfo([
-            {
-              reviews: [
-                res.data.data[0].review,
-                "nice xa",
-                "hasgdf ashkdgfkjhasgdf gaskhdfg agdfhgsdf agdfkhgahdgfkjhasd fhgahdsgfjhasg fjadgfhjagdfhgadhfgjhadgfjhagdfhgashdfghajsdgfjhagdfjhgajdhsgfjhadgfjhagdfjhgahjdfgjadgfjhagdfjhgsdjf",
-                "haina horw",
-                "hok",
-                "tait ",
-                "hovanya",
-              ],
-              rating: res.data.data[0].rating,
-              reviewer: res.data.data[0].user.Username,
-              productId: res.data.data[0].product,
-              MainImage:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQor6LAYPUWKAO7T4VGjvLLIJxv5uB4ii7lbw&usqp=CAU",
-              Name: "Hami",
-            },
-            {
-              reviews: [res.data.data[0].review, "nice xa"],
-              rating: res.data.data[0].rating,
-              reviewer: res.data.data[0].user.Username,
-              productId: res.data.data[0].product,
-              MainImage:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQor6LAYPUWKAO7T4VGjvLLIJxv5uB4ii7lbw&usqp=CAU",
-              Name: "Hami",
-            },
-            {
-              reviews: [res.data.data[0].review, "nice xa"],
-              rating: res.data.data[0].rating,
-              reviewer: res.data.data[0].user.Username,
-              productId: res.data.data[0].product,
-              MainImage:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQor6LAYPUWKAO7T4VGjvLLIJxv5uB4ii7lbw&usqp=CAU",
-              Name: "Hami",
-            },
-          ]);
-          return { ...res.data.data, ...result.data.data[0] };
+          return res.data.data;
         } catch (err) {
           console.log(err);
         }
@@ -74,7 +49,7 @@ const SellerReviews = () => {
   return (
     <div className={styles.review}>
       {productReviewInfo?.map((s) => {
-        return <SellerReviewComponent key={s.productId} data={s} />;
+        return <SellerReviewComponent key={s.id} data={s} />;
       })}
     </div>
   );
